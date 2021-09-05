f83d377341b5e60a36178dcdae8ebf2dca177c2f
AhmedMahmood <abo.thabet693@gmail.com>
2021-09-05 15:11:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,116 @@
+const navBtns = document.querySelectorAll('.nav-link');
+const menuBtn = document.querySelector('#menu-btn');
+const linkContainer = document.querySelector('#nav-container');
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+const productsSlide = document.querySelector('#products-slide');
+const slideContainer = document.querySelector('#slide-container');
+let productCards = [...document.querySelectorAll('.product-card')];
+
+
+var l = 0;
+
+
+
+// let sliderWidth = slideContainer.offsetWidth;
+let sliderWidth =1500;
+let cardWidth = productCards[0].offsetWidth
+console.log(sliderWidth);
+console.log(cardWidth);
+
+let value = 0;
+
+
+
+// const tl = gsap.timeline({defaults: {duration: 0.6, ease:"power2.inOut"}});
+//
+// tl.from(".bg", {x: "-100%", opacity: 0})
+//   .from("p", {opacity: 0}, "-=0")
+//   .from("h1", {opacity: 0, y: "30px"}, "-=0.3")
+//   .from("button", {opacity: 0, y: "-40px"}, "-=0.8")
+
+
+
+function updateDom(arr) {
+  productsSlide.innerHTML =''
+  arr.forEach((card) => {productsSlide.append(card)});
+}
+
+let f =  sliderWidth/(5);
+let sc = sliderWidth/(5);
+
+function moveLeft(S){
+  sc <= 0 ? sc=sliderWidth : sc -= f;
+  console.log(slideContainer.scrollLeft)
+  slideContainer.scroll(sc,0)
+
+}
+
+
+function moveRight(S){
+  slideContainer.scroll(sc,0)
+  sc >= sliderWidth ? sc=0 : sc += f;
+  console.log(sliderWidth,cardWidth,value);
+  console.log(sc);
+  productsSlide.style.transform = `translateX(-${value}px)`;
+    clearInterval(start)
+  start = setInterval(()=>{
+    moveRight(value)
+  },3000);
+}
+
+let start = setInterval(()=>{
+  moveRight(value);
+},3000);
+
+
+
+right.addEventListener('click',() => {
+  // clearInterval(start)
+  moveRight(value);
+});
+
+left.addEventListener('click',() => {
+  // clearInterval(start)
+  moveLeft(value);
+});
+
+
+
+navBtns.forEach((btn) => {
+
+  btn.addEventListener('click',(e)=>{
+    showHide()
+    navBtns.forEach((btn) =>{
+        btn.classList.remove('act');
+    });
+    console.log(btn);
+    btn.classList.add('act');
+
+  });
+});
+
+function changeIcone() {
+  if (menuBtn.classList.contains('fa-bars')){
+    menuBtn.className = 'fas fa-times';
+  } else {
+    menuBtn.className = 'fas fa-bars';
+  }
+}
+
+function showHide() {
+  {
+    linkContainer.classList.toggle('showed');
+
+    changeIcone();
+
+  }
+}
+
+menuBtn.addEventListener('click',showHide);
+
+
+
+
+
+
